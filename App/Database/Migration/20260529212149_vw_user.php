48f700794ad6516f0d617bce17f78e756260ab32
ryanvasq325 <ryanvasques77@gmail.com>
2026-06-01 18:28:39 -0400

    Commit

--- App/Database/Migration/20260529212149_vw_user.php
+++ App/Database/Migration/20260529212149_vw_user.php
@@ -15,8 +15,39 @@ final class Version20260529212149 extends AbstractMigration
     }
 
     public function up(Schema $schema): void
-    {
-        // escreva aqui as alterações
+    { {
+            $this->addSql(<<<'SQL'
+            CREATE OR REPLACE VIEW vw_user AS
+            SELECT
+                u.id,
+                u.nome,
+                u.sobrenome,
+                u.cpf,
+                u.rg,
+                u.senha,
+                u.ativo,
+                u.administrador,
+                MAX(c.contato) FILTER (WHERE c.tipo = 'EMAIL')    AS email,
+                MAX(c.contato) FILTER (WHERE c.tipo = 'CELULAR')  AS celular,
+                MAX(c.contato) FILTER (WHERE c.tipo = 'TELEFONE') AS telefone,
+                u.criado_em,
+                u.atualizado_em
+            FROM public.users u
+            LEFT JOIN public.contact c
+                   ON c.id_usuario = u.id
+            GROUP BY
+                u.id,
+                u.nome,
+                u.sobrenome,
+                u.cpf,
+                u.rg,
+                u.senha,
+                u.ativo,
+                u.administrador,
+                u.criado_em,
+                u.atualizado_em
+        SQL);
+        }
     }
 
     public function down(Schema $schema): void

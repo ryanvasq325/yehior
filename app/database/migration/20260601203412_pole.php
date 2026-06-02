69ae4ac441de0cf72d6e48fa8d2dd669ecfe2129
AllanBS429 <allbatista@hotmail.com>
2026-06-01 21:06:04 -0400

    ok

---
+++ app/database/migration/20260601203412_pole.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Database\Migration;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20260601203412 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'pole';
+    }
+
+    public function up(Schema $schema): void
+    {
+      $table = $schema->createTable('pole');
+        $table->addColumn('id',            'bigint',           ['autoincrement' => true, 'notnull' => true]);
+        $table->addColumn('id_bairro',       'bigint',           [ 'notnull'      => false]);
+        $table->addColumn('numero',        'string',           ['length'        => 255,  'notnull' => true]);
+        $table->addColumn('criado_em',     'datetime',         ['notnull'       => true, 'default' => 'CURRENT_TIMESTAMP']);
+        $table->addColumn('atualizado_em', 'datetime',         ['notnull'       => true, 'default' => 'CURRENT_TIMESTAMP']);
+
+        $table->addForeignKeyConstraint('address', ['id_bairro'], ['id'], ['onDelete' => 'RESTRICT', 'onUpdate' => 'CASCADE'], 'fk_pole_bairro');
+
+    }
+
+    public function down(Schema $schema): void
+    {
+        // escreva aqui o rollback do up()
+    }
+}
---
+++ app/database/migration/20260601203505_address.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Database\Migration;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20260601203505 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'address';
+    }
+
+    public function up(Schema $schema): void
+    {
+         $table = $schema->createTable('address');
+
+        $table->addColumn('id',            'bigint',  ['autoincrement' => true,  'notnull' => true]);
+        $table->addColumn('id_city',       'bigint',  [ 'notnull' => false]);
+        $table->addColumn('id_customer',   'bigint',  [ 'notnull' => false]);
+        $table->addColumn('id_supplier',   'bigint',  [ 'notnull' => false]);
+        $table->addColumn('id_enterprise', 'bigint',  [ 'notnull' => false]);
+        $table->addColumn('logradouro',    'string',  ['length' => 255, 'notnull' => true]);
+        $table->addColumn('bairro',        'string',  ['length' => 100, 'notnull' => true]);
+        $table->addColumn('cep',           'string',  ['length' => 10,  'notnull' => true]);
+        $table->addColumn('numero',        'integer', ['notnull' => true]);
+        $table->addColumn('complemento',   'string',  ['length' => 100, 'notnull' => false]);
+        $table->addColumn('criado_em',     'datetime', ['notnull' => true, 'default' => 'CURRENT_TIMESTAMP']);
+        $table->addColumn('atualizado_em', 'datetime', ['notnull' => true, 'default' => 'CURRENT_TIMESTAMP']);
+
+        $table->setPrimaryKey(['id']);
+        $table->addIndex(['id_city']);
+        $table->addIndex(['id_customer']);
+        $table->addIndex(['id_supplier']);
+        $table->addIndex(['id_enterprise']);
+
+        $table->addForeignKeyConstraint('city',       ['id_city'],       ['id'], ['onDelete' => 'RESTRICT', 'onUpdate' => 'CASCADE'], 'fk_address_city');
+        $table->addForeignKeyConstraint('customer',   ['id_customer'],   ['id'], ['onDelete' => 'RESTRICT', 'onUpdate' => 'CASCADE'], 'fk_address_customer');
+        $table->addForeignKeyConstraint('supplier',   ['id_supplier'],   ['id'], ['onDelete' => 'RESTRICT', 'onUpdate' => 'CASCADE'], 'fk_address_supplier');
+        $table->addForeignKeyConstraint('enterprise', ['id_enterprise'], ['id'], ['onDelete' => 'RESTRICT', 'onUpdate' => 'CASCADE'], 'fk_address_enterprise');
+    }
+    
+
+    public function down(Schema $schema): void
+    {
+        // escreva aqui o rollback do up()
+    }
+}
